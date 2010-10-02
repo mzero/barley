444f2af25907314b50331fbc0c2316e6887bfa57
Johan Tibell <tibell@tibell-macbookpro.local>
2010-10-02 11:18:28 -0400

    Added index.hs

--- Index.hs
+++ Index.hs
@@ -3,8 +3,9 @@ module Template where
 import Text.Html
 
 page =
-    thehtml (
-        header (
-             thelink ! [href "public/scaffold.css", rel "stylesheet", thetype "text/css"] noHtml
-             ) +++
-        h1 (toHtml "Welcome aboard!"))
+    thehtml <<
+    header << [
+        thelink ! [href "public/scaffold.css", rel "stylesheet",
+                   thetype "text/css"] << noHtml,
+        h1 << "Welcome aboard!"
+        ]
