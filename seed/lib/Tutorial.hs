e584838470bfa8b02b17298e7c8c19e15b96dde2
Mark Lentczner <markl@glyphic.com>
2011-02-08 21:21:35 -0800

    better tutorial step links

--- seed/lib/Tutorial.hs
+++ seed/lib/Tutorial.hs
@@ -21,30 +21,47 @@ module Tutorial(
     ) where
 
 import Control.Monad (join)
+import Data.List (groupBy)
 import Text.Html
 
 data StepType = Slides | Code
-data Step = Step { stepUrl :: String, stepType :: StepType }
+data Step = Step { stepCh, stepSt :: Int, stepType :: StepType }
 
-stepLink :: Step -> Html
-stepLink (Step u t) = anchor ! [href $ "source?file=" ++ u ++ ".hs" ++ p t] << u
+stepUrl :: Step -> String
+stepUrl s = "Chapter" ++ cn ++ "/Step_" ++ cn ++ "_" ++ sh
+  where
+   cn = show $ stepCh s
+   sh = show $ stepSt s
+
+stepHref :: Step -> String
+stepHref s = "source?file=" ++ stepUrl s ++ ".hs" ++ p (stepType s)
   where
     p Code = ""
     p Slides = "&preview=1"
 
+stepLongName :: Step -> String
+stepLongName (Step ch st _) = "Chapter " ++ show ch ++ ", Step " ++ show st
+
+stepShortName :: Step -> String
+stepShortName (Step _ st _ ) = "Step " ++ show st
+
+stepLink :: Step -> Html
+stepLink s = anchor ! [href $ stepHref s] << stepLongName s
+
+
 -- | The list of URL paths that are in the tutorial, in order.
 steps :: [Step]
 steps =
-    [ Step "Chapter1/Step_1_1" Slides
-    , Step "Chapter1/Step_1_2" Code
-    , Step "Chapter1/Step_1_3" Slides
-    , Step "Chapter1/Step_1_4" Code
-    , Step "Chapter1/Step_1_5" Slides
-    , Step "Chapter1/Step_1_6" Code
-    , Step "Chapter1/Step_1_7" Code
-    , Step "Chapter2/Step_2_1" Slides
-    , Step "Chapter2/Step_2_2" Code
-    , Step "Chapter2/Step_2_3" Code
+    [ Step 1 1 Slides
+    , Step 1 2 Code
+    , Step 1 3 Slides
+    , Step 1 4 Code
+    , Step 1 5 Slides
+    , Step 1 6 Code
+    , Step 1 7 Code
+    , Step 2 1 Slides
+    , Step 2 2 Code
+    , Step 2 3 Code
     ]
 
 stepUrls :: [String]
@@ -83,4 +100,12 @@ tutorialModule t =
     linkTo = maybe (toHtml "--none--") stepLink
     
 tutorialOutlineModule :: Html
-tutorialOutlineModule = (h2 << "Tutorial") +++ unordList (map stepLink steps)
+tutorialOutlineModule = (h2 << "Tutorial") +++ map section groupedSteps
+  where
+    groupedSteps = groupBy (\a b -> stepCh a == stepCh b) steps
+    section ss@((Step ch _ _):_) = heading +++ list
+      where
+        heading = p << ("Chapter " ++ show ch)
+        list = unordList (map stepLink ss)
+        stepLink s = anchor ! [href $ stepHref s] << stepShortName s
+
