8c7047f2678993c4352bd5737b9c24eb4820b3e2
Mark Lentczner <markl@glyphic.com>
2011-02-24 19:32:21 -0800

    tweaked display hack

--- seed/Source.js
+++ seed/Source.js
@@ -77,7 +77,7 @@ var mkEdit = function() {
     preview.slideUp('fast');
     editor.slideDown('fast', function() {
         cmEditor.refresh();
-        setTimeout(function(){cmEditor.refresh();}, 50); // HACK!
+        setTimeout(function(){cmEditor.refresh();}, 0); // HACK!
         adjustColumnMarker();
     });
     toggle(editImage, runImage);
